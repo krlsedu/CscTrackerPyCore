d60e0b11e6f1ca9a2f1fc68088acd43b4956ca43
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2024-02-04 12:52:57 -0300

    Add request save option to Interceptor class
    The Interceptor class has been expanded to include an optional feature that saves request information. If activated, it collects request details such as path, method, headers, parameters, and more. Then, the data is sent via HTTP repository to a designated address for storage and further usage. In addition, the related Flask app instantiation has also been adjusted accordingly in Starter class.

--- csctracker_py_core/starter.py
+++ csctracker_py_core/starter.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+from csctracker_queue_scheduler.services.scheduler_service import SchedulerService
 from flask import Flask
 from flask_cors import CORS
 from flask_swagger_generator.generators.swagger_view import SwaggerView
@@ -15,7 +16,7 @@ from csctracker_py_core.utils.version import Version
 
 
 class Starter:
-    def __init__(self, static_folder="static"):
+    def __init__(self, static_folder="static", save_request=False):
         self.logger = logging.getLogger()
         self.app = Flask(__name__, static_folder=static_folder)
         try:
@@ -26,12 +27,13 @@ class Starter:
             os.environ.pop("FLASK_ENV")
         except:
             pass
+        SchedulerService.init(10)
         self.cors = CORS(self.app)
         self.app.config['CORS_HEADERS'] = 'Content-Type'
         self.config = Configs(os.getenv('PROFILE', 'dev'))
-        self.interceptor = Interceptor(self.app)
         self.remote_repository = RemoteRepository()
         self.http_repository = HttpRepository(remote_repository=self.remote_repository)
+        self.interceptor = Interceptor(self.app, self.http_repository, save_request=save_request)
         self.metrics = PrometheusMetrics(
             self.app,
             group_by='endpoint',

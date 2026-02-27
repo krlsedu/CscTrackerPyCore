f20d709804555ab790e452d1eb289de9a58286af
Carlos Eduardo Duarte Schwalm (krlsedu) <krlsedu@gmail.com>
2026-02-27 09:15:53 -0300

    Add utility methods to fetch request headers, arguments, JSON body, and API token

--- csctracker_py_core/utils/request_info.py
+++ csctracker_py_core/utils/request_info.py
@@ -3,6 +3,8 @@ import uuid
 
 from flask import g, request
 
+from csctracker_py_core.models.emuns.config import Config
+from csctracker_py_core.utils.configs import Configs
 from csctracker_py_core.utils.version import Version
 
 
@@ -29,3 +31,31 @@ class RequestInfo:
         return request.headers.get(
             "x-correlation-id", f"{Version.get_app_name()}-{str(uuid.uuid4())}"
         )
+
+    @staticmethod
+    def get_header(header_name: str):
+        _headers = request.headers
+        _header = next((v for k, v in _headers.items() if k.lower() == header_name.lower()), None)
+        return _header
+
+    @staticmethod
+    def get_api_token():
+        return Configs.get_env_variable(Config.API_TOKEN)
+
+    @staticmethod
+    def get_json_body():
+        return request.json
+
+    @staticmethod
+    def get_args():
+        return request.args
+
+    @staticmethod
+    def get_headers():
+        headers = request.headers
+        headers_ = {}
+        for key in headers.keys():
+            headers_[key] = headers.get(key)
+        if "x-correlation-id" not in headers_:
+            headers_["x-correlation-id"] = RequestInfo.get_request_id(True)
+        return headers
